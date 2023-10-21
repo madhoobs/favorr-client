a8ec24d379994fd5ac5b27b29750059e0f59fe91
Hasan Almadhoob <hasan@madhoob.com>
2023-10-21 04:34:30 +0300

    Add basic register

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ const App = () => {
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login setUser={setUser} />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/register" element={<Register setUser={setUser} />} />
       </Routes>
     </div>
   )
