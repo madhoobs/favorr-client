5ab8d700e7911df97556eacb530e3b4eb689fad9
Abbas Lutfalla <abbas00must@gmail.com>
2023-10-24 15:13:49 +0300

    update the services

--- src/services/Order.js
+++ src/services/Order.js
@@ -1,8 +1,16 @@
 import Client from './api'
 
-export const GetOrderByUser = async (favorId) => {
+export const GetOrderByUser = async (userID) => {
   try {
     const res = await Client.get(`/order/${userID}`)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+export const GetOrder = async (orderId) => {
+  try {
+    const res = await Client.get(`/order/find/${orderId}`)
     return res.data
   } catch (error) {
     throw error
@@ -11,7 +19,7 @@ export const GetOrderByUser = async (favorId) => {
 
 export const CreateOrder = async (data) => {
   try {
-    const res = await Client.post(`/add/:packageID`, data)
+    const res = await Client.post(`/add/packageID`, data)
   } catch (error) {
     throw error
   }
