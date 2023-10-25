3107cab773a5b02add98d143ecf5f785308b0577
Hasan Almadhoob <hasan@madhoob.com>
2023-10-25 23:05:50 +0300

    Improve homepage layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { GetCategory } from '../services/Category'
 import CategoryCard from '../components/CategoryCard'
+import '../style/home'
+import '../style/home.css'
 
 const Home = () => {
   const [categories, setCategories] = useState([])
@@ -26,6 +28,23 @@ const Home = () => {
 
   return (
     <div>
+      <div className="background">
+        <div className="header">
+          <div className="headerWrapper">
+            <div className="logo-wrapper" alt="logo">
+              <img className="logo" src="/logo-w.png" alt="Favorr Logo" />
+            </div>
+            <br />
+            <h1>
+              Freelancing <br /> Redefined
+            </h1>
+            <div className="circle blue"></div>
+            <div className="square red"></div>
+            <div className="triangle green"></div>
+            <div className="circle orange"></div>
+          </div>
+        </div>
+      </div>
       <CategoryCard categories={categories} onClick={onClick} />
     </div>
   )
