77940cbf0bf36bfd2c8b7011ad9e69c37dda6ec9
sbahlool <saud.bahlool@hotmail.com>
2023-10-25 20:40:08 +0300

    added styling to forms

--- src/components/PackageForm.jsx
+++ src/components/PackageForm.jsx
@@ -1,22 +1,28 @@
 const PackageForm = ({ handleChangePackage, newPackage }) => {
   return (
     <div>
-      <h1>Add A Package</h1>
+      <h3>Add A Package</h3>
+      <label className="label">Price:</label>
       <input
+        className="input"
         type="number"
         value={newPackage.price}
         onChange={handleChangePackage}
         name={'price'}
         placeholder={'price'}
       />
+      <label className="label">Tier:</label>
       <input
+        className="input"
         type="text"
         value={newPackage.tier}
         onChange={handleChangePackage}
         name={'tier'}
         placeholder={'tier'}
       />
+      <label className="label">Description:</label>
       <input
+        className="input"
         type="text-area"
         value={newPackage.description}
         onChange={handleChangePackage}
